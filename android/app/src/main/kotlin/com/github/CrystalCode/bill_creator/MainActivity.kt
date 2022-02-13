fb70bd3c710cebb2d60a342036b63470d96a3897
Sanket <ProgrammerSanket@gmail.com>
2022-02-13 09:29:17 +0530

    Working Done on UI FrontEnd

---
+++ android/app/src/main/kotlin/com/github/CrystalCode/bill_creator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.CrystalCode.bill_creator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: App(),
+  ));
+}
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Padding(
+              padding: EdgeInsets.only(bottom: 9.0),
+              child: Text("दख्खन सप्लायर्स अपशिंगे",
+                style: TextStyle(fontSize: 34),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Text("Address - At Post Apshinge tal koregoan Dist Satara.",
+              style: TextStyle(fontSize: 14,color: Colors.black54),
+              textAlign: TextAlign.center,
+            ),
+            Text("Phone No - 8208553219",
+              style: TextStyle(fontSize: 14,color: Colors.black54),
+              textAlign: TextAlign.center,
+            ),
+            Text("Email - vaibhavtate002@gmail.com",
+              style: TextStyle(fontSize: 14,color: Colors.black54),
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top:8.0),
+              child: ElevatedButton(
+                onPressed: sampleFunction,
+                child: Text('Create Bill'),
+              ),
+            )
+          ],
+        ),
+      )
+    ));
+  }
+}
+
+
+sampleFunction(){
+
+  print('Function on Click Event Called.');
+  // Put your code here, which you want to execute on onPress event.
+}
