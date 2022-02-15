732433a0bcde05e78180b07b10d190fd8498ff1f
Sanket <ProgrammerSanket@gmail.com>
2022-02-15 13:33:39 +0530

    Working done on navigation one screen to another

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+
 import 'package:bill_creator/Pages/Invoice.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Invoice(),
+    home: App(),
   ));
 }
 
@@ -22,34 +23,36 @@ class _AppState extends State<App> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Padding(padding: EdgeInsets.only(bottom: 9.0),
-              child: Image(image: AssetImage('assets/images/logo.png'),width: 50,height: 50,),
+          children:  [
+            const Padding(padding: EdgeInsets.only(bottom: 9.0),
+              child: Image(image: AssetImage('assets/images/logo.png'),width: 150,height: 150,),
             ),
-            Padding(
+            const Padding(
               padding: EdgeInsets.only(bottom: 9.0),
               child: Text("दख्खन सप्लायर्स अपशिंगे",
                 style: TextStyle(fontSize: 34),
                 textAlign: TextAlign.center,
               ),
             ),
-            Text("Address - At Post Apshinge tal koregoan Dist Satara.",
+            const Text("Address - At Post Apshinge tal koregoan Dist Satara.",
               style: TextStyle(fontSize: 14,color: Colors.black54),
               textAlign: TextAlign.center,
             ),
-            Text("Phone No - 8208553219",
+            const Text("Phone No - 8208553219",
               style: TextStyle(fontSize: 14,color: Colors.black54),
               textAlign: TextAlign.center,
             ),
-            Text("Email - vaibhavtate002@gmail.com",
+            const Text("Email - vaibhavtate002@gmail.com",
               style: TextStyle(fontSize: 14,color: Colors.black54),
               textAlign: TextAlign.center,
             ),
             Padding(
-              padding: EdgeInsets.only(top:8.0),
+              padding: const EdgeInsets.only(top:8.0),
               child: ElevatedButton(
-                onPressed: sampleFunction,
-                child: Text('Create Bill'),
+                onPressed: (){
+                  Navigator.push(context,MaterialPageRoute(builder: (context) => const Invoice()),);
+                },
+                child: const Text('Create Bill'),
               ),
             )
           ],
@@ -60,8 +63,3 @@ class _AppState extends State<App> {
 }
 
 
-sampleFunction(){
-
-  print('Function on Click Event Called.');
-  // Put your code here, which you want to execute on onPress event.
-}
